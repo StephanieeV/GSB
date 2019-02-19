ba823a7f3b7f3485964f871b34ce573e6e89e67c
stef_ <stef_@DESKTOP-0NNOCP5>
2019-02-19 16:54:54 +0100

    Ajout de l'en-tete pour le comptable Ajout de la vue v_validerFicheFrais

--- vues/v_accueilComptable.php
+++ vues/v_accueilComptable.php
@@ -37,11 +37,11 @@
                         <a href="index.php?uc=gererFrais&action=saisirFrais"
                            class="btn btn-success btn-lg" role="button">
                             <span class="glyphicon glyphicon-pencil"></span>
-                            <br>Renseigner la fiche de frais</a>
+                            <br>Valider les fiches de frais</a>
                         <a href="index.php?uc=etatFrais&action=selectionnerMois"
                            class="btn btn-primary btn-lg" role="button">
                             <span class="glyphicon glyphicon-list-alt"></span>
-                            <br>Afficher mes fiches de frais</a>
+                            <br>Suivre le paiement des fiches de frais</a>
                     </div>
                 </div>
             </div>
